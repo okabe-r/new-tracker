51cb7f57b381797844b9921065cc2a0a8e3287fe
okabe-r <66364246+okabe-r@users.noreply.github.com>
2025-08-22 22:07:13 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+// A name for our cache
+const CACHE_NAME = 'progress-tracker-cache-v1';
+
+// The list of files to cache.
+const urlsToCache = [
+  './',
+  './index.html',
+  'https://raw.githubusercontent.com/okabe-r/image/refs/heads/main/Telegram_GH6IQqemtI.png?token=GHSAT0AAAAAAC77URNENIV7GRA64ILLIPOM2FIX4LA'
+];
+
+// 1. On install, open the cache and add the core application files.
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 2. On fetch, try to serve from the cache first.
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // If we found a match in the cache, return it.
+        if (response) {
+          return response;
+        }
+        // Otherwise, fetch it from the network.
+        return fetch(event.request);
+      })
+  );
+});
